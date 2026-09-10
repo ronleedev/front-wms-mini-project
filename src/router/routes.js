f4f0f31b6c15967179fce58a30e2aa91209bd2fa
sharon <sharon329@moasystems.co.kr>
2026-09-10 16:04:50 +0900

    feat: 재고 조정

--- src/router/routes.js
+++ src/router/routes.js
@@ -10,6 +10,8 @@ const routes = [
       { path: 'plan', component: () => import('pages/PlanPage.vue') },
       { path: 'recv', component: () => import('pages/ReceiptPage.vue') },
       { path: 'stock', component: () => import('pages/StockPage.vue') },
+      { path: 'stockMove', component: () => import('pages/StockMovePage.vue') },
+      { path: 'stockAdjMove', component: () => import('pages/StockAdjPage.vue') },
     ]
   }
 ]
